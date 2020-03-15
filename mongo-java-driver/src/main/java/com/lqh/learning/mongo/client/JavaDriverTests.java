4618288f51673ff8fba2a07f45303e4357e4fa28
liqxhx <liqxhx@gmail.com>
2020-03-15 18:30:59 +0800

    CRUD

--- mongo-java-driver/src/main/java/com/lqh/learning/mongo/client/JavaDriverTests.java
+++ mongo-java-driver/src/main/java/com/lqh/learning/mongo/client/JavaDriverTests.java
@@ -1,5 +1,6 @@
 package com.lqh.learning.mongo.client;
 
+import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -19,9 +20,33 @@ import org.junit.Test;
  * @since 2020/03/14 09:55
  */
 public class JavaDriverTests {
+    @Test
+    public void testCurd() {
+        Mongo mongo = new com.mongodb.MongoClient("localhost", 27017);
+
+        DB db = new DB(mongo, "cc");
+
+        DBCollection dbCollection = db.getCollection("t_operate_records");
+
+        for (int i = 0; i < 10; i++) {
+            DBObject dbObject = new BasicDBObject();
+            dbObject.put("id", UUID.randomUUID().toString());
+            dbObject.put("action", i%2==0? "add" : "modify");
+            dbObject.put("time", System.nanoTime());
+            dbObject.put("content", "do something "+ i);
+
+            dbCollection.insert(dbObject);
+        }
+
+        DBCursor dbCursor = dbCollection.find();
+        for (DBObject obj : dbCursor) {
+            System.out.println(obj.toString());
+        }
+
+    }
 
     @Test
-    public void test1(){
+    public void testNativeAPI(){
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
 
         MongoDatabase mongoDatabase = mongoClient.getDatabase("mongo-learning");
